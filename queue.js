104e7d57e3f4a2878ecba189d69b4580c86bead9
Imad M <imad@acentium.com>
2020-01-21 11:59:25 +0100

    add terminal dep

--- queue.js
+++ queue.js
@@ -1,5 +1,7 @@
 const random = require("random");
+const Queue = require("../terminal/terminal");
 
-let x = random.int(0, 100); // 72
+// let x = random.int(0, 100); // 72
 
-console.log(x);
+// console.log(x);
+Queue.sayHellow();
